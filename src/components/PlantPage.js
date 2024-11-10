9ff421daae7e74f6b93cc69057bbb241081077a3
Hakim Waithaka <hakim.waithaka@gmail.com>
2024-11-10 22:52:48 +0300

    by Hakim

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,28 @@
-import React from "react";
+// PlantPage.js
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((response) => response.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={(newPlant) => setPlants([...plants, newPlant])} />
+      <Search search={search} setSearch={setSearch} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
